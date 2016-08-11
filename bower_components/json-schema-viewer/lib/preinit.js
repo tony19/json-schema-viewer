58705f9b1afd8ca1143a999ba5c2c6c571e9fe06
Tony Trinh <tony19@gmail.com>
2016-08-10 22:05:40 -0500

    Remove bower_components from checkin

--- bower_components/json-schema-viewer/lib/preinit.js
+++
@@ -1,8 +0,0 @@
-//If using JQM, you must handle permalinks *before* initializing JQM
-(function($) {
-  var hash = window.location.hash,
-      path = hash.match(/v=([^\&]+)/);
-
-  window.jsvInitPath = path ? path[1] : false;
-  window.location.hash = hash.match(/#[^\?]+/) || '';
-})(jQuery);
